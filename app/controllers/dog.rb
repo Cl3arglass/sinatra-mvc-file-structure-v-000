5e98c5bae1d4178a0ce132921f331d8c31615441
Holden Altaffer <haltaffer@gmail.com>
2018-06-13 21:19:05 +0000

    automatically backed up by learn

--- app/controllers/dog.rb
+++ app/controllers/dog.rb
@@ -3,9 +3,15 @@ class Dog
   attr_accessor :name, :age
   attr_reader :breed
 
+  @@all = []
+
   def initialize(name, breed, age)
     @name = name;
     @breed = breed;
-    @age = age; 
+    @age = age;
+  end
+
+  def self.all
+    @@all << self;
   end
 end
